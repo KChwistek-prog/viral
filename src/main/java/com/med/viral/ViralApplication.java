95cbc79e0f69e750dd41255d95fc35de35a8359b
Krzysztof <k.chwistek@outlook.com>
2024-09-10 09:58:32 +0200

    initial commit - clean app

---
+++ src/main/java/com/med/viral/ViralApplication.java
@@ -0,0 +1,13 @@
+package com.med.viral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/med/viral/ViralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.med.viral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

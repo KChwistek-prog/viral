a05be919d1b607a616e244bc8b83431185803a3b
Krzysztof <k.chwistek@outlook.com>
2024-09-12 12:07:10 +0200

    appointment repository

---
+++ src/main/java/com/med/viral/repository/AppointmentRepository.java
@@ -0,0 +1,9 @@
+package com.med.viral.repository;
+
+import com.med.viral.model.Appointment;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppointmentRepository extends CrudRepository<Appointment, Long> {
+}

c328f7db398b074f30793b034402cb4cc54e688d
Krzysztof <k.chwistek@outlook.com>
2024-09-16 10:38:36 +0200

    remove old repository and service for patient/doctor/admin

--- src/main/java/com/med/viral/repository/DoctorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.med.viral.repository;
-
-import com.med.viral.model.Doctor;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DoctorRepository extends CrudRepository<Doctor, Integer> {
-}

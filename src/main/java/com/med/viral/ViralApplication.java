a903207f67c5c08e18c242caba321b4d73969893
Krzysztof <k.chwistek@outlook.com>
2024-09-16 11:25:46 +0200

    gradle dependencies and default admin creation

--- src/main/java/com/med/viral/ViralApplication.java
+++ src/main/java/com/med/viral/ViralApplication.java
@@ -1,13 +1,34 @@
 package com.med.viral;
 
+import com.med.viral.model.security.RegisterRequest;
+import com.med.viral.security.AuthenticationService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import static com.med.viral.model.security.Role.ADMIN;
 
 @SpringBootApplication
 public class ViralApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ViralApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ViralApplication.class, args);
+    }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(
+            AuthenticationService service
+    ) {
+        return args -> {
+            var admin = RegisterRequest.builder()
+                    .firstname("Admin")
+                    .lastname("Admin")
+                    .email("admin@mail.com")
+                    .password("password")
+                    .role(ADMIN)
+                    .build();
+            System.out.println("Admin token: " + service.register(admin).accessToken());
+        };
+    }
 }

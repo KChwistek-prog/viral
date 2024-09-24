16131becf1e7a9c973ac9aaab7f182956583bd9a
Krzysztof <k.chwistek@outlook.com>
2024-09-24 10:17:12 +0200

    field mapping fix

--- src/main/java/com/med/viral/model/mapper/UserMapper.java
+++ src/main/java/com/med/viral/model/mapper/UserMapper.java
@@ -3,9 +3,13 @@ package com.med.viral.model.mapper;
 import com.med.viral.model.DTO.UserDTO;
 import com.med.viral.model.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-    User toEntity(UserDTO userDTO);
+    @Mapping(source = "accountNonLocked", target = "isAccountNonLocked")
     UserDTO toDTO(User user);
+
+    @Mapping(source = "isAccountNonLocked", target = "isAccountNonLocked")
+    User toEntity(UserDTO userDTO);
 }

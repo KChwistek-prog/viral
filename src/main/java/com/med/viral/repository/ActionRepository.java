003eca6abc3548ce97646fbb509487fd1aa64651
Krzysztof <k.chwistek@outlook.com>
2024-09-30 09:46:38 +0200

    @Repository removed

--- src/main/java/com/med/viral/repository/ActionRepository.java
+++ src/main/java/com/med/viral/repository/ActionRepository.java
@@ -2,8 +2,6 @@ package com.med.viral.repository;
 
 import com.med.viral.model.Action;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ActionRepository extends JpaRepository<Action, Integer> {
 }
--- src/main/java/com/med/viral/repository/AppointmentRepository.java
+++ src/main/java/com/med/viral/repository/AppointmentRepository.java
@@ -2,9 +2,7 @@ package com.med.viral.repository;
 
 import com.med.viral.model.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
 
 }
